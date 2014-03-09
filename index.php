6771e968739b1fc321aea3043996b8fcbedad94b
jujuvan <julienvan4@hotmail.fr>
2014-03-09 19:53:02 +0100

    modify liste pays

--- index.php
+++ index.php
@@ -3,29 +3,30 @@ require_once( "sparqllib.php" );
  
 $db = sparql_connect( "http://dbpedia.org/sparql" );
 if( !$db ) { print sparql_errno() . ": " . sparql_error(). "\n"; exit; }
-sparql_ns( "foaf","http://xmlns.com/foaf/0.1/" );
+
  
-$sparql = "SELECT DISTINCT ?Country WHERE { ?Country a <http://dbpedia.org/ontology/Country> . }";
-$result = sparql_query( $sparql ); 
-if( !$result ) { print sparql_errno() . ": " . sparql_error(). "\n"; exit; }
+$sparql = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX yago: <http://dbpedia.org/class/yago/>
+PREFIX dbpedia-owl: <http://dbpedia.org/ontology/>
+ 
+SELECT ?place WHERE {
+    ?place rdf:type yago:EuropeanCountries
+}";
+
+$result = sparql_query( $sparql );  
+
+if( !$result ) { print sparql_errno() . ": " . sparql_error(). "\n"; exit; } 
  
 $fields = sparql_field_array( $result );
  
 print "<p>Number of rows: ".sparql_num_rows( $result )." results.</p>";
-print "<table class='example_table'>";
-print "<tr>";
-foreach( $fields as $field )
-{
-	print "<th>$field</th>";
-}
-print "</tr>";
+echo "<select name='pays'>";
+
 while( $row = sparql_fetch_array( $result ) )
 {
-	print "<tr>";
 	foreach( $fields as $field )
 	{
-		print "<td>$row[$field]</td>";
+		print "<option>$row[$field]";
 	}
-	print "</tr>";
 }
-print "</table>";
+echo "</select>";
