20ac0641e5c16ce952653a1f29e1060291686a15
Grégoire <julien_78400@hotmail.fr>
2014-03-14 00:01:17 +0100

    correction condition

--- www/controleur/statistique.php
+++ www/controleur/statistique.php
@@ -1,1 +1,1 @@
-<?phpinclude('modele/statistique.php');if(isset($_POST['choixnbpays'])){	$nombrepays = $_POST['nbpays'];}$listepays = getlistepays();include('vue/structuresite.php');include('vue/statistique.php');
+<?phpinclude('modele/statistique.php');if(isset($_POST['choixnbpays'])){	$nombrepays = $_POST['nbpays'];	$listepays = getlistepays();}include('vue/structuresite.php');include('vue/statistique.php');
